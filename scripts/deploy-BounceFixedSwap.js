dced217311fddde66aadf035328d4d16c8399229
rbtcool <rbtcoool@yahoo.com>
2021-05-23 19:50:06 +0800

    update

--- scripts/deploy-BounceFixedSwap.js
+++ scripts/deploy-BounceFixedSwap.js
@@ -3,7 +3,7 @@ const { ethers, upgrades } = require("hardhat");
 async function main() {
     const [ owner ] = await ethers.getSigners();
     const BounceFixedSwap = await ethers.getContractFactory("BounceFixedSwap");
-    const fs = await upgrades.deployProxy(BounceFixedSwap, [owner.address], 'initialize');
+    const fs = await upgrades.deployProxy(BounceFixedSwap, [], 'initialize');
     await fs.deployed();
 
     console.log("BounceFixedSwap deployed to:", fs.address);
